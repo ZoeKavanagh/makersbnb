6d11326acbc92d3b77eca0dd59510c4865bbfda5
Dione Estabillo <dione.estabillo@gmail.com>
2018-05-21 18:16:18 +0100

    Added feature tests for header contents

--- spec/views/index_spec.rb
+++ spec/views/index_spec.rb
@@ -2,10 +2,29 @@ feature 'index page' do
 
   context 'on loading' do
 
-    scenario 'css loads correctly' do
-      # expect(page).to have_content 'makersbnb'
+    before do
+      visit '/'
     end
 
+    scenario 'logo loads' do
+      expect(page).to have_css("img[src*='./images/logo.png']")
+    end
+
+    scenario 'title loads' do
+      expect(page).to have_content('makersbnb')
+    end
+
+    scenario 'home menu item is visible' do
+      expect(page).to have_css('a#home-menu-item')
+    end
+
+    scenario 'add room menu item is visible' do
+      expect(page).to have_css('a#add-room-menu-item')
+    end
+
+    scenario 'reserve room menu item is visible' do
+      expect(page).to have_css('a#reserve-room-menu-item')
+    end
   end
 
 end
