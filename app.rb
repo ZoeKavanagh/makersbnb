111a14408e4273b49919bb4d455cf4571872d6d7
Zoe Kavanagh <zoekavanagh2017@hotmail.com>
2018-05-23 16:29:07 +0100

    more availablity stuff and testing

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require './lib/booking'
 require './lib/room'
 require './lib/user'
+require './lib/availability'
 require 'data_mapper'
 require 'sinatra'
 require 'sinatra/base'
@@ -44,14 +45,19 @@ class Makersbnb < Sinatra::Base
   end
 
   post '/rooms/new' do
-    Room.create(
-        name: params[:name],
-        location: params[:location],
-        description: params[:description],
-        from: params[:start_date],
-        to: params[:end_date],
-        user_id: '1',
-      )
+    new_room = Room.create(
+      name: params[:name],
+      location: params[:location],
+      description: params[:description],
+      from: params[:start_date],
+      to: params[:end_date],
+      user_id: '1',
+    )
+    all_dates = Availability.dates_calculation(
+      params[:start_date],
+      params[:end_date]
+    )
+    Availability.create_dates(all_dates, new_room.id)
     redirect '/rooms/requests'
   end
 
--- spec/features/add_room_spec.rb
+++ spec/features/add_room_spec.rb
@@ -7,8 +7,8 @@ feature 'adding a room' do
   let(:name) { 'Champagne Central' }
   let(:location) { 'London' }
   let(:description) { 'Booooooze!' }
-  let(:start_date) { '01/11/2018' }
-  let(:end_date) { '30/11/2018' }
+  let(:start_date) { '2018-11-29' }
+  let(:end_date) { '2018-11-30' }
 
   scenario 'can successfully complete form' do
     rooms_fill_in_and_submit(name, location, description, start_date, end_date)
@@ -21,12 +21,14 @@ feature 'adding a room' do
     expect(Room.all.last.name).to eq name
     expect(Room.all.last.location).to eq location
     expect(Room.all.last.description).to eq description
-    expect(Room.all.last.from.strftime("%d/%m/%Y")).to eq start_date
-    expect(Room.all.last.to.strftime("%d/%m/%Y")).to eq end_date
+    expect(Room.all.last.from.strftime("%Y-%m-%d")).to eq start_date
+    expect(Room.all.last.to.strftime("%Y-%m-%d")).to eq end_date
   end
 
-  xscenario 'availability data added to database' do
-    number_of_dates = Date.strptime(end_date, "%d").day - Date.strptime(start_date, "%d").day
+  scenario 'availability data added to database' do
+    Date.strptime(end_date, "%Y-%m-%d")
+    Date.strptime(start_date, "%Y-%m-%d")
+    number_of_dates = Date.strptime(end_date, "%Y-%m-%d").day - Date.strptime(start_date, "%Y-%m-%d").day + 1
     rooms_fill_in_and_submit(name, location, description, start_date, end_date)
     expect(Availability.all.last).to be_instance_of(Availability)
     expect(Availability.all.last.date).to eq Date.strptime(end_date, "%Y-%m-%d")
